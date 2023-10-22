4b96deacffc16523e2ccad39531cab64feaaf315
Vijay Yalla, yallav, Vijay, vyalla <vijayakumar_yalla@yahoo.com>
2023-10-22 16:50:38 +0100

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+variable "region" {
+    default = "us-east-2"
+}
+
+terraform {
+    required_providers {
+
+    }
+}
+
+provider "aws" {
+  
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0df435f331839b2d6"
+  instance_type          = "t2.micro"
+  tags = {
+    Name = "first-tf-instance"
+  }
+}
