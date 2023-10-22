6a84724289bfd8f1888067928753adabd47d75ff
Vijay Yalla, yallav, Vijay, vyalla <vijayakumar_yalla@yahoo.com>
2023-10-22 17:20:48 +0100

    Updated files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 variable "region" {
-    default = "us-east-2"
+  default = "us-east-2"
 }
 
-provider "aws" { }
+provider "aws" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df435f331839b2d6"
-  instance_type          = "t2.micro"
+  ami           = "ami-0df435f331839b2d6"
+  instance_type = "t2.micro"
   tags = {
     Name = "first-tf-instance"
   }
