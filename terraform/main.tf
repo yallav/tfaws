a571fd2ffaf37600fc0eb25c92e5f439919c91be
Vijay Yalla, yallav, Vijay, vyalla <vijayakumar_yalla@yahoo.com>
2023-10-23 13:40:45 +0100

    Added instance commandline variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,6 @@ resource "aws_instance" "web" {
   ami           = "ami-0df435f331839b2d6"
   instance_type = "t2.micro"
   tags = {
-    Name = "first-tf-instance"
+    Name = var.instancename
   }
 }
